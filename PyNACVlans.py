195bc22b51541b25887d4cc714739efc6e6a239c
Alex La Fazia <alex.j.lafazia@gmail.com>
2021-03-11 08:19:04 -0800

    Update PyNACVlans.py
    Added new logging feature
    Added write memory command
    Fixed username as string not variable

--- PyNACVlans.py
+++ PyNACVlans.py
@@ -1,9 +1,13 @@
 from netmiko import ConnectHandler
-import getpass
+import getpass, re, logging
+
+# Logging section ##############
+logging.basicConfig(filename="logger.log", level=logging.DEBUG)
+logger = logging.getLogger("netmiko")
 
 def AddL2NACVlan(ip, usr, paswd):
 
-    net_connect = ConnectHandler(device_type='hp_procurve', ip=ip, username='usr', password=paswd)
+    net_connect = ConnectHandler(device_type='hp_procurve', ip=ip, username=usr, password=paswd)
 
     tagged = net_connect.send_command("show run vlan 999 | inc tagged | ex untagged")
 
@@ -22,17 +26,19 @@ def AddL2NACVlan(ip, usr, paswd):
     output1 = net_connect.send_config_set(vlan50)
     output2 = net_connect.send_config_set(vlan66)
     output3 = net_connect.send_config_set(vlan70)
-    output4 = net_connect.send_config_set(vlan70)
+    output4 = net_connect.send_config_set(vlan999)
+    output5 = net_connect.send_config_set('write mem')
 
     print (output)
     print (output1)
     print (output2)
     print (output3)
     print (output4)
+    print (output5)
 
 def AddL3NACVlan(ip, usr, paswd):
     
-    net_connect = ConnectHandler(device_type='hp_procurve', ip=ip, username='usr', password=paswd)
+    net_connect = ConnectHandler(device_type='hp_procurve', ip=ip, username=usr, password=paswd)
 
     tagged = net_connect.send_command("show run vlan 999 | inc tagged | ex untagged")
     ipHelper = net_connect.send_command("show run vlan 999 | inc ip helper")
@@ -62,12 +68,14 @@ def AddL3NACVlan(ip, usr, paswd):
     output2 = net_connect.send_config_set(vlan50)
     output3 = net_connect.send_config_set(vlan66)
     output4 = net_connect.send_config_set(vlan70)
+    output5 = net_connect.send_config_set('write mem')
 
     print (output)
     print (output1)
     print (output2)
     print (output3)
     print (output4)
+    print (output5)
 
 def AddL2NACPorts(ip, usr, paswd):
 
